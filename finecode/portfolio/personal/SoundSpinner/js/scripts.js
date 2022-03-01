8e01d11afcdc0f627895af95841f9c99dfdb3796
acieffe <acieffe@gmail.com>
2022-02-28 21:12:40 -0500

    Added different animals

--- finecode/portfolio/personal/SoundSpinner/js/scripts.js
+++ finecode/portfolio/personal/SoundSpinner/js/scripts.js
@@ -1,60 +1,77 @@
 let spinner = document.querySelector('.spinner');
-let btn = document.getElementById('spin');
+let spin = document.getElementById('spin');
+let btnOne = document.getElementById('setOne');
+let btnTwo = document.getElementById('setTwo');
+let btnThree = document.getElementById('setThree');
+let btnHidden = document.getElementById('hidden');
 let rotations = 0;
 let degrees = 0;
-let output = document.querySelector('.output');
+var currentSet = ['turkey', 'horse', 'sheep', 'cat', 'duck', 'dog', 'rooster', 'cow'];
 
-btn.onclick = function () {
+btnOne.onclick = function () {
+	currentSet = ['turkey', 'horse', 'sheep', 'cat', 'duck', 'dog', 'rooster', 'cow'];
+	changeImages(currentSet);
+};
+
+btnTwo.onclick = function () {
+	currentSet = ['whale', 'lion', 'elephant', 'dinosaur', 'hippo', 'zebra', 'tiger', 'panda'];
+	changeImages(currentSet);
+};
+
+btnThree.onclick = function () {
+	currentSet = ['hyena', 'otter', 'owl', 'snake', 'bee', 'wolf', 'falcon', 'frog'];
+	changeImages(currentSet);
+};
+
+btnHidden.onclick = function () {
+	currentSet = ['penguin', 'penguin', 'penguin', 'penguin', 'penguin', 'penguin', 'penguin', 'penguin'];
+	changeImages(currentSet);
+};
+
+spin.onclick = function () {
 	rotations += Math.ceil(Math.random() * 720) + 720;
 	spinner.style.transform = 'rotate(' + rotations + 'deg)';
-   degrees = rotations % 360;
-   makeSound(degrees);
+	degrees = rotations % 360;
+	makeSound(degrees);
 };
 
 function makeSound(degrees) {
-	var tile = 0;
 	setTimeout(function () {
 		if (degrees <= 45) {
-			tile = 8;
-			var audio = new Audio('sounds/cow.mp3');
+			var audio = new Audio('sounds/' + currentSet[7] + '.mp3');
 			audio.play();
-		}
-      else if (degrees <= 90) {
-			tile = 7;
-			var audio = new Audio('sounds/rooster.mp3');
+		} else if (degrees <= 90) {
+			var audio = new Audio('sounds/' + currentSet[6] + '.mp3');
 			audio.play();
-		}
-      else if (degrees <= 135) {
-			tile = 6;
-			var audio = new Audio('sounds/dog.mp3');
+		} else if (degrees <= 135) {
+			var audio = new Audio('sounds/' + currentSet[5] + '.mp3');
 			audio.play();
-		}
-      else if (degrees <= 180) {
-			tile = 5;
-			var audio = new Audio('sounds/duck.mp3');
+		} else if (degrees <= 180) {
+			var audio = new Audio('sounds/' + currentSet[4] + '.mp3');
 			audio.play();
-		}
-      else if (degrees <= 225) {
-			tile = 4;
-			var audio = new Audio('sounds/cat.mp3');
+		} else if (degrees <= 225) {
+			var audio = new Audio('sounds/' + currentSet[3] + '.mp3');
 			audio.play();
-		}
-      else if (degrees <= 270) {
-			tile = 3;
-			var audio = new Audio('sounds/sheep.mp3');
+		} else if (degrees <= 270) {
+			var audio = new Audio('sounds/' + currentSet[2] + '.mp3');
 			audio.play();
-		}
-      else if (degrees <= 315) {
-			tile = 2;
-			var audio = new Audio('sounds/horse.mp3');
+		} else if (degrees <= 315) {
+			var audio = new Audio('sounds/' + currentSet[1] + '.mp3');
 			audio.play();
-		}
-      else {
-			tile = 1;
-			var audio = new Audio('sounds/turkey.mp3');
+		} else {
+			var audio = new Audio('sounds/' + currentSet[0] + '.mp3');
 			audio.play();
 		}
 	}, 5000); // 5 seconds
-} 
+}
 
-
+function changeImages(currentSet) {
+	document.getElementById('one').src = 'img/' + currentSet[0] + '.png';
+	document.getElementById('two').src = 'img/' + currentSet[1] + '.png';
+	document.getElementById('three').src = 'img/' + currentSet[2] + '.png';
+	document.getElementById('four').src = 'img/' + currentSet[3] + '.png';
+	document.getElementById('five').src = 'img/' + currentSet[4] + '.png';
+	document.getElementById('six').src = 'img/' + currentSet[5] + '.png';
+	document.getElementById('seven').src = 'img/' + currentSet[6] + '.png';
+	document.getElementById('eight').src = 'img/' + currentSet[7] + '.png';
+}
